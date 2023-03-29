0edd9d94d0a81d8ae87c9c3a73cc7707d310888f
Adam Young <adamyoungy678@gmail.com>
2023-03-29 10:46:59 +0100

    verifying if the password is the same as in the database

--- passwordhash.php
+++ passwordhash.php
@@ -5,3 +5,11 @@
 $dbPassword = password_hash('banana', PASSWORD_DEFAULT);
 
 // Store $dbPassword (the hash of the actual password) in a database
+
+// Let's check the password - pretend the user is logging in
+
+// Get the hashed password from the database
+
+$correct = password_verify('apple', $dbPassword);
+
+var_dump($correct);
